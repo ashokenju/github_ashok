228d1aa2b7aa586744403b90a3a213478176cb72
ashokenju9 <ashokenju9@gmail.com>
2024-04-18 16:36:42 +0530

    16:36-commit

--- variable.sh
+++ variable.sh
@@ -3,10 +3,10 @@
 person1=Raheem
 person2=robert
 
-echo "$person1:How are you Ramesh?"
+echo "$person1:How are you $person2?"
 
-echo  "$person2:fine suresh how are you"
+echo  "$person2:fine $person1 how are you"
 
-echo "$person1:fine ramesh,How is your work"
+echo "$person1:fine $person2,How is your work"
 
 echo "$person2:Not bad i am planning to learn devops"
