44b98505a5449276581b6f3ac944b7fd21c99d16
ashokenju9 <ashokenju9@gmail.com>
2024-04-18 18:32:28 +0530

    18:32-connit

---
+++ 8_spec_variable.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "All variable :$@"
+
+echo "No of varabiles passed : $#"
+
+echo "script name: $0"
+
+echo "current working direcotry : $PWD"
+
+echo "Home directory is :$HOME"
