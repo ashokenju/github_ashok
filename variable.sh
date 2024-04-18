0c3a4019a483103b674329723ab750d0a8c0d4bc
ashokenju9 <ashokenju9@gmail.com>
2024-04-18 16:04:51 +0530

    16:04-commit

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "suresh:How are you Ramesh?"
+
+echo  "Ramesh:fine suresh how are you"
+
+echo "suresh:fine ramesh,How is your work"
+
+echo "Ramesh:Not bad i am planning to learn devops"
