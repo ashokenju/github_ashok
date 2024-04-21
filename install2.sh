c684eae7ac4543e9dc797b147d314338e734bc1b
ashokenju9 <ashokenju9@gmail.com>
2024-04-21 17:06:39 +0530

    1706-commit

---
+++ install2.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+p1=$(id -u)
+
+if [ $p1 -ne 0 ]  
+
+then
+
+echo "you are not root user"
+
+exit 1  #exit if a not root user.
+
+else 
+
+echo "You are super user"
+
+fi
+
+dnf install mysql-server  -y
+
+if [ $? -ne  0 ]; then
+ 
+ echo "mysql-server installation Not Success"
+
+exit 1
+ else
+ 
+ echo "mysql-server installed successfully"
+
+ 
+
+ fi
+
+
