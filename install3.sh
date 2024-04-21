9fdc3d16da612b576403687dacffbe8a3c3ddc6e
ashokenju9 <ashokenju9@gmail.com>
2024-04-21 18:21:18 +0530

    1821-commit

--- install3.sh
+++ install3.sh
@@ -2,7 +2,7 @@
 
  p1=$(id -u)
  script_name=$(echo $? |  cut -d '.' -f1)
- date_format=date_time=$(date +"%d%m%Y_%H%M")
+ date_format=$(date +"%d%m%Y_%H%M")
  log_path=/tmp/$script_name_$date_format.log
 
  #path=$(script_name=$(echo $? |  cut -d '.' -f1)_date_format=date_time=$(date +"%d%m%Y_%H%M")_)
