d4aaeddfeac0a86cf5318bf60032973e5d304baa
ashokenju9 <ashokenju9@gmail.com>
2024-04-21 18:04:42 +0530

    1804-commit

---
+++ install3.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+ p1=$(id -u)
+ script_name=$(echo $? |  cut -d '.' -f1)
+ date_format=date_time=$(date +"%d%m%Y_%H%M")
+ log_path=/tmp/$script_name_$date_format.log
+
+ #path=$(script_name=$(echo $? |  cut -d '.' -f1)_date_format=date_time=$(date +"%d%m%Y_%H%M")_)
+ #function-validate
+
+ validate()  {
+
+   if [ $1 -ne  0 ]
+
+  then
+
+   echo "$2 is failure"
+
+ else
+
+  echo "$2 is success"
+
+  fi
+
+ }
+
+ dnf install mysql-server  -y  &>$log_path
+
+ validate $? "mysql-servr installation"
+
+
+
+
